54f35bb863f3102fe5a02081919a57de5e7f1ff9
Tendo <tendof59@gmail.com>
2021-12-14 22:44:38 -0500

    subiendo

---
+++ js/main.js
@@ -0,0 +1,58 @@
+window.sr = ScrollReveal();
+	sr.reveal('header',{
+		duration:2000,
+		
+	});
+	sr.reveal('.carousel',{
+		duration:2000,
+		
+	});
+
+	sr.reveal('.colegio',{
+		duration:2000,
+		origin: 'right',
+		distance: '-100px'
+	});
+	sr.reveal('.depa',{
+		duration:2000,
+		origin: 'left',
+		distance: '-100px'
+	});
+	sr.reveal('.fibro',{
+		duration:2000,
+		origin: 'right',
+		distance: '-100px'
+	});
+	sr.reveal('.derecha',{
+		duration:2000,
+		origin: 'left',
+		distance: '-100px'
+	});
+	sr.reveal('.izquierda',{
+		duration:2000,
+		origin: 'right',
+		distance: '-100px'
+	});
+	sr.reveal('.icono',{
+		duration:1500,
+		origin: 'left',
+		distance: '-350px'
+	});
+	sr.reveal('.primera_linea',{
+		duration:2000,
+	});
+	sr.reveal('.hogar',{
+		duration:1500,
+		origin: 'bottom',
+		distance: '-350px'
+	});
+	sr.reveal('.tienda',{
+		duration:1500,
+		origin: 'top',
+		distance: '-350px'
+	});
+	sr.reveal('.oficina',{
+		duration:1500,
+		origin: 'bottom',
+		distance: '-350px'
+	});
